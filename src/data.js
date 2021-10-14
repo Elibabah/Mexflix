547aee9757d9059fa90e7e51a0c7cdfd3808fe30
Elibabah <delias16.hernandez@gmail.com>
2021-10-14 12:11:42 -0500

    borrador de funciones

--- src/data.js
+++ src/data.js
@@ -1,2 +1,35 @@
 // ----------------------------- FUNCIONES DE RENDEREADO API -----------------------------------//
-// ------------ OBTE__NER DATOS  DE LA API Y EXPORTARLOS  PARA USARLOS EN APP.JS ---------------//
+// ------------ OBTENER DATOS  DE LA API Y EXPORTARLOS  PARA USARLOS EN APP.JS ---------------//
+
+
+// Función para renderear películas
+let renderingPokemon = (pokemonData) => {
+    let pokemonName = document.getElementById("pokemon-name")
+    let pokemonImg = document.getElementById("pokemon-img")
+    pokemonName.innerHTML = pokemonData.name
+    pokemonImg.setAttribute("src", pokemonData.sprites.front_default)
+}
+
+
+export let getPokemonfromAPI = (pokemonParaBuscar) => {
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + `${pokemonParaBuscar}`) //no es una string, es una variable por las comillas invertidas ``
+
+    .then((response) => {
+        console.log(response)
+        if (response.status == 404) {
+            alert("ese pokemon NO EXISTE; ¿seguro que se escribe así?")
+        } else {
+            response.json()
+                .then((data) => renderingPokemon(data))
+                .catch((error) => console.log(error))
+        }
+    })
+
+    /*.then((data) => renderingPokemon(data))
+
+    .catch((error) => console.log(error))*/
+
+    .finally(() => console.log("promesas resueltas"))
+
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 // ------------------------------------- D O M ------------------------------------------//
 // ------------ OBTENER VALORES DE BÚSQUEDA PARA PROCESARLOS EN APP.JS ----------------- //
+
+
+
+//funtion to be triggered from search button
+let searchPokemon = () => {
+    //bring input value
+    let searchValue = document.getElementById("searchInput").value
+    getPokemonfromAPI(searchValue)
+}
+
+//bring button to give afunction
+let buttonSearch = document.getElementById("searchButton")
+buttonSearch.addEventListener("click", searchPokemon)
