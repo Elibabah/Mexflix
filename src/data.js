79d34e02ecfc89ea6364db4b4a4d2ff4fcb34fa5
Elibabah <delias16.hernandez@gmail.com>
2021-10-14 12:00:36 -0500

    cambios con gitignore y archivos

--- src/data.js
+++ src/data.js
@@ -1,1 +1,2 @@
 // ----------------------------- FUNCIONES DE RENDEREADO API -----------------------------------//
+// ------------ OBTE__NER DATOS  DE LA API Y EXPORTARLOS  PARA USARLOS EN APP.JS ---------------//
