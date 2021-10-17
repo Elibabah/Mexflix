978a003bb60cf4194a26beb842165fcb1982e7b8
Elibabah <delias16.hernandez@gmail.com>
2021-10-17 16:04:01 -0500

    intentando traer catálogo

--- src/data.js
+++ src/data.js
@@ -1,8 +1,94 @@
 // ----------------------------- FUNCIONES DE RENDEREADO API -----------------------------------//
 // ------------ OBTENER DATOS  DE LA API Y EXPORTARLOS  PARA USARLOS EN APP.JS ---------------//
 
+/*
+//--------------------------------------- C A T Á L O G O -------------------------------//
+
+let traerMuestra1 = (movieData) => {
+    console.log(movieData)
+    let películaNombre = document.getElementById("nombre")
+    let películaAño = document.getElementById("año")
+    let películaDirector = document.getElementById("director")
+    let películaPoster = document.getElementById("poster")
+
+    películaNombre.innerHTML = movieData.Title // Traer nombre
+    películaAño.innerHTML = movieData.Year // TRAER AÑO
+    películaDirector.innerHTML = movieData.Director //Director
+    películaPoster.setAttribute("src", movieData.Poster) // Traer póster
+}
+
+
+
+export let traerMuestraPorAPI = () => { // SE EXPORTA DESDE API CON getMoviefromAPI que coincide con IMPORTACIÓN
+
+    //P R O M E S A S
+
+    fetch("https://www.omdbapi.com/?i=tt3896198&apikey=cbd9e008" + "&apikey=cbd9e008")
+        .then((response) => { //-----------------------------P R O M E S A 
+            console.log(response)
+            response.json()
+                .then((data) => traerMuestra1(data)) // ----------PROMESA
+                .catch((error) => console.log(error)) // ----------CANALIZAR ERROR DE BÚSQUEDA
+        })
+        .finally(() => console.log("promesa resueltas")) // -------------FIN DE PROMESAS 
+}
+
+*/
+//----------------------------------- B Ú S Q U E D A ----------------------------------//
+
 //Funciones para renderear (obtener) película
 let renderingMovie = (movieData) => {
+    console.log(movieData)
+    let movieName = document.getElementById("movie-name")
+    let movieImg = document.getElementById("movie-img")
+    let movieYear = document.getElementById("movie-year")
+    let movieActors = document.getElementById("movie-actors")
+    let movieReleased = document.getElementById("movie-released")
+    let movieGenre = document.getElementById("movie-genre")
+    let movieDirector = document.getElementById("movie-director")
+    let movieLanguage = document.getElementById("movie-language")
+    let movieRuntime = document.getElementById("movie-runtime")
+
+    //ACTIVAR LAS VARIABLES (Título, poster, año, actores, director)
+
+    movieName.innerHTML = movieData.Title // TRAER TÍTULO DE PELÍCULA
+    movieImg.setAttribute("src", movieData.Poster) // TRAER PÓSTER
+    movieYear.innerHTML = movieData.Year // TRAER AÑO
+    movieActors.innerHTML = movieData.Actors // Traer actores 
+    movieReleased.innerHTML = movieData.Released //fecha lanzamiento
+    movieGenre.innerHTML = movieData.Genre // Género
+    movieDirector.innerHTML = movieData.Director //Director
+    movieLanguage.innerHTML = movieData.Language //Lenguage
+    movieRuntime.innerHTML = movieData.Runtime //Duración
+}
+
+export let getMoviefromAPI = (movieParaBuscar) => { // SE EXPORTA DESDE API CON getMoviefromAPI que coincide con IMPORTACIÓN
+
+    //P R O M E S A S
+
+    fetch("https://www.omdbapi.com/?t= '' + ''" + `${movieParaBuscar}` + "&apikey=cbd9e008")
+        .then((response) => { //-----------------------------P R O M E S A 
+            console.log(response)
+            if (response.status == 404) { //------- RESPUESTA A ERROR 404
+                alert("Esa película no existe. Por favor, revisa el nombre");
+            } else {
+                response.json()
+                    .then((data) => renderingMovie(data)) // ----------PROMESA
+                    .catch((error) => console.log(error)) // ----------CANALIZAR ERROR DE BÚSQUEDA
+            }
+        })
+
+    .finally(() => console.log("promesas resueltas")) // -------------FIN DE PROMESAS 
+
+}
+
+/*
+
+//--------------------------------------- INTENTANDO TREAER AÑO -------------------------------//
+
+
+//Funciones para renderear (obtener) año
+let renderingYear = (movieData) => {
     console.log(movieData)
     let movieName = document.getElementById("movie-name")
     let movieImg = document.getElementById("movie-img")
@@ -26,18 +112,20 @@ let renderingMovie = (movieData) => {
     movieLanguage.innerHTML = movieData.Language //Lenguage
 }
 
-export let getMoviefromAPI = (movieParaBuscar) => { // SE EXPORTA DESDE API CON getMoviefromAPI que coincide con IMPORTACIÓN
+
+
+export let getYearfromAPI = (añoParaBuscar) => { // SE EXPORTA DESDE API CON getMoviefromAPI que coincide con IMPORTACIÓN
 
     //P R O M E S A S
 
-    fetch("https://www.omdbapi.com/?t= '' + ''" + `${movieParaBuscar}` + "&apikey=cbd9e008")
+    fetch("https://www.omdbapi.com/?y=" + `${añoParaBuscar}` + "&apikey=cbd9e008")
         .then((response) => { //-----------------------------P R O M E S A 
             console.log(response)
             if (response.status == 404) { //------- RESPUESTA A ERROR 404
-                alert("Esa película no existe. Por favor, revisa el nombre");
+                alert("No hay películas con ese año o el año tiene un error");
             } else {
                 response.json()
-                    .then((data) => renderingMovie(data)) // ----------PROMESA
+                    .then((data) => renderingYear(data)) // ----------PROMESA
                     .catch((error) => console.log(error)) // ----------CANALIZAR ERROR DE BÚSQUEDA
             }
         })
@@ -45,3 +133,5 @@ export let getMoviefromAPI = (movieParaBuscar) => { // SE EXPORTA DESDE API CON 
     .finally(() => console.log("promesas resueltas")) // -------------FIN DE PROMESAS 
 
 }
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 // ------------------------------------- D O M ------------------------------------------//
 // ------------ OBTENER VALORES DE BÚSQUEDA PARA PROCESARLOS EN APP.JS ----------------- //
+
+// Pantallas
 document.getElementById("screen1").hidden = false //Mostrar pantalla 1
 document.getElementById("screen2").hidden = true //Ocultar pantalla 2
-document.getElementById("screen3").hidden = true //Ocultar pantalla 3
 
-// Intentando poner imagen de acceso
+// Ícono de usuario para acceso
 let acceso = document.getElementById("imagen-acceso")
 acceso.addEventListener("click", function() {
     document.getElementById("screen1").hidden = true
     document.getElementById("screen2").hidden = false
 })
 
+/*
+// ------------------------------ C A T Á L O G O --------------------------------------//
 
+import { traerMuestraPorAPI } from "./data.js"; // "getMoviefromAPI" debe coincidir tannto en IMPORT como en EXPORT
+function(imprimirMovie) {
+    imprimirMovie("muestra1").innerHTML = traerMuestraPorAPI
+}*/
 
+//------------------------------ TRAER POR TÍTULO ------------------------------------//
 
 // Importar películas desde API. Recibe el EXPORT de data.js
 import { getMoviefromAPI } from "./data.js"; // "getMoviefromAPI" debe coincidir tannto en IMPORT como en EXPORT
@@ -29,3 +37,23 @@ let searchMovie = () => {
 //traer botón del HTML por ID para darle función
 let buttonSearch = document.getElementById("searchButton")
 buttonSearch.addEventListener("click", searchMovie) //Escuchador de evento para que al "click" se enlace a la función
+
+/*
+//--------------------------- TRAER POR AÑO ---------------------------------------//
+
+// Importar películas por año desde API. Recibe el EXPORT de data.js
+import { getYearfromAPI } from "./data.js"; // "getYearfromAPI" debe coincidir tannto en IMPORT como en EXPORT
+
+
+
+// función que se activará desde el botón de búsqueda
+let searchYear = () => {
+    //traer valor de entrada
+    let searchValue = document.getElementById("searchInputYear").value
+    getYearfromAPI(searchValue)
+}
+
+//traer botón del HTML por ID para darle función
+let buttonSearch = document.getElementById("searchButtonYear")
+buttonSearch.addEventListener("click", searchYear) //Escuchador de evento para que al "click" se enlace a la función
+*/
